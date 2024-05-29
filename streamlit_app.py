d76cc071c830afc3f6aa8c3a0c6e38fa7dd14e48
esv2024 <162897102+esv2024@users.noreply.github.com>
2024-05-29 16:35:29 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,11 @@ import streamlit as st
 """
 # Welcome to Streamlit!
 
+# Load the CSV file to inspect its contents
+file_path = '/mnt/data/file-9qrHea2KAD8NUpDkGUa6vQP5'
+data = pd.read_csv(file_path)
+data.head()
+
 # Sort the data by the 'Votes' column to find the movies with the least votes
 sorted_data = data.sort_values(by='Votes', ascending=True)
 sorted_data.head()
