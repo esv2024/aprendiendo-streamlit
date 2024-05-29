7cbada5539d3654092e97f4f19c14bfb303457db
esv2024 <162897102+esv2024@users.noreply.github.com>
2024-05-29 16:48:35 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,8 +7,7 @@ import streamlit as st
 # Welcome to Streamlit!
 
 # Load the CSV file to inspect its contents
-file_path = 'aprendiento-streamlit/IMDB-Movie-Data.csv'
-data = pd.read_csv(file_path)
+data = pd.read_csv("IMDB-Movie-Data.csv")
 # data.head()
 
 # Sort the data by the 'Votes' column to find the movies with the least votes
